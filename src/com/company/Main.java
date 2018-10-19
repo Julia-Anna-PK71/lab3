9aead350f01b7eee14a016d3244e8535a1e50c61
Юля <yukaangel324@gmail.com>
2018-10-19 10:44:29 +0300

    add lab3

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int C3;
+        C3= 7117%3;
+        System.out.println(C3);
+        int C17;
+        C17= 7117%17;
+        System.out.println(C17);
+        // write your code here
+        // C3=1; StringBuffer
+        // C17=11; Із заданого тексту видалити всі слова визначеної довжини,що починаються з приголосної літери.
+    }
+}
